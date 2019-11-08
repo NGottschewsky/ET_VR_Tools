99347aedc9cc49a8a5d71903402a7cf9e0036422
NGottschewsky <nina.gottschewsky@gmail.com>
2019-11-08 13:45:06 +0100

    New Prefabs added

---
+++ Assets/Scripts/CueController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CueController : MonoBehaviour
+{
+    public TextMeshProUGUI cue;
+    public Color32 cueColour;
+
+    public void ShowText(string msg)
+    {
+        cue.text = msg;
+        cue.color = cueColour;
+    }
+}
---
+++ Assets/Scripts/ToolPresenter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolPresenter : MonoBehaviour
+{
+    public GameObject screwdriver;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

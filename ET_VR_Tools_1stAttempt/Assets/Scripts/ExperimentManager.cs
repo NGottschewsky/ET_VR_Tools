27aaccb2cdc6fb381af4b0775744a76161ec0e18
NGottschewsky <nina.gottschewsky@gmail.com>
2019-11-11 15:47:16 +0100

    Experiment Scene basics

---
+++ ET_VR_Tools_1stAttempt/Assets/Scripts/ExperimentManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class ExperimentManager : MonoBehaviour
+{
+    [Header("Experiment components")] 
+    public TextMeshProUGUI cue;
+    public CueController cueController;
+    private List<string> _cues;
+    
+    private void Start()
+    {
+        Debug.Log(string.Format("Reached"));
+        _cues = new List<string> {"Lift", "Use"};
+        cue = new TextMeshProUGUI();
+       //"Welcome to the Experiment.", _cueColour);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.Log(string.Format("Reached Point2"));
+        cueController.ShowText("Welcome to the Experiment.");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cueController.ShowText(_cues[Mathf.FloorToInt(Random.Range(0, _cues.Count))]);
+        }
+        Debug.Log(string.Format("Reached Point3"));
+    }
+}
---
+++ ET_VR_Tools_1stAttempt/Assets/Scripts/ToolPresenter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ToolPresenter : MonoBehaviour
+{
+    public GameObject tool;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(tool);
+        }
+    }
+}

3ec11f2ecf6928dde3acdb5e633db4cf3de0b36b
NGottschewsky <nina.gottschewsky@gmail.com>
2019-11-14 12:34:10 +0100

    ToolController and ToolManager scripts added.

---
+++ ET_VR_Tools_2ndAttempt/Assets/Scripts/ToolManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolManager : MonoBehaviour
+{
+    private List<ToolController> _tools = new List<ToolController>();
+    private int _trial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trial = 0;
+        foreach(GameObject toolPrefab in GameObject.FindGameObjectsWithTag("Tool"))
+        {
+            _tools.Add(toolPrefab.GetComponent<ToolController>());
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
--- ET_VR_Tools_2ndAttempt/Assets/Scripts/ToolPresenter.cs
+++ ET_VR_Tools_2ndAttempt/Assets/Scripts/ToolPresenter.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class ToolPresenter: MonoBehaviour
 {
     public GameObject[] toolPrefabs;
-    private Vector3 position;
-    private Quaternion rotation;
+    private Vector3 _position;
+    private Quaternion _rotation;
+    //this should be an attribute of another class of which the tools are objects
+    private bool _ispresent = false;
 
     private void Start()
     {
-        position = new Vector3(0.5f, 1.549906f,0.0f);
-        rotation = new Quaternion(0,0,0,0);
+        //position at centre of table and directly on top of it
+        _position = new Vector3(0.5f, 1.549906f,0.0f);
+        _rotation = new Quaternion(0,0,0,0);
     }
 
     void presentTool(GameObject tool)
     {
-        // add position and rotation to Instantiated tool
-        GameObject toolInstance = Instantiate(tool, position, rotation).GetComponent<GameObject>();
+        //if 1 tool is already on the table, it shouldn't be able to spawn another
+        if (!_ispresent)
+        {
+            GameObject toolInstance = Instantiate(tool, _position, _rotation).GetComponent<GameObject>();
+        } 
     }
 
     // Update is called once per frame
@@ -26,9 +32,13 @@ public class ToolPresenter: MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             presentTool(toolPrefabs[0]);
+            //1 tool is already on the table
+            _ispresent = true;
         } else if (Input.GetKeyDown(KeyCode.A))
         {
             presentTool((toolPrefabs[1]));
+            //1 tool is already on the table
+            _ispresent = true;
         }
     }
 }

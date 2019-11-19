f609264b5d60216b6b247b0ce31e004b13123b27
NGottschewsky <nina.gottschewsky@gmail.com>
2019-11-19 16:08:38 +0100

    ToolPresenter und ToolManager bearbeitet

---
+++ ET_VR_Tools_2ndAttempt/Assets/Scripts/ToolManager2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ToolManager2 : MonoBehaviour
+{
+    
+    private List<ToolController> _tools = new List<ToolController>();
+    private int _trial;
+    public GameObject[] tools;
+
+    //private ToolPresenter toolPresenter = gameObject.AddComponent<ToolPresenter>;
+    //private ToolPresenter _toolPresenter = new ToolPresenter(tools);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trial = 0;
+        
+        //Add all tools, which I've tagged as 'Tool'
+        foreach(GameObject toolPrefab in GameObject.FindGameObjectsWithTag("Tool"))
+        {
+            _tools.Add(toolPrefab.GetComponent<ToolController>());
+        }
+    }
+    
+    
+    public void GetNextTool(out ToolController nextTool)
+    {
+        foreach (var tool in _tools)
+        {
+            if (tool.id.Equals(toolOrder[_trial]))
+            {
+                nextTool = tool;
+            }
+        }
+// Das ist noch nicht so das wahre hier
+        nextTool = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var tool in tools)
+        {
+            ToolPresenter.INSTANCE.presentTool(tool);
+        }
+    }
+}
---
+++ ET_VR_Tools_2ndAttempt/Assets/Scripts/ToolPresenter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ToolPresenter: MonoBehaviour
+{
+    
+    public GameObject[] toolPrefabs;
+    private Vector3 position;
+    private Quaternion rotation;
+    private bool _isPresent;
+
+    
+    public static readonly ToolPresenter INSTANCE = new ToolPresenter();
+
+    private ToolPresenter()
+    {
+    }
+    
+    public static ToolPresenter Instance
+    {
+        get { return INSTANCE; }
+    }
+
+
+    private void Start()
+    {
+        position = new Vector3(0.5f, 1.549906f,0.0f);
+        rotation = new Quaternion(0,0,0,0);
+        _isPresent = false;
+    }
+
+    
+    public void presentTool(GameObject tool)
+    {
+        if (!_isPresent)
+        {
+            // add position and rotation to Instantiated tool
+            GameObject toolInstance = Instantiate(tool, position, rotation).GetComponent<GameObject>();
+        }
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            presentTool(toolPrefabs[0]);
+            _isPresent = true;
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            presentTool((toolPrefabs[1]));
+            _isPresent = true;
+        }
+    }
+    
+   }
+
+

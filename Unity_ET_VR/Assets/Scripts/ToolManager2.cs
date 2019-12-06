7e48e6e2f18d4f970de0c4fed259e1d3ab3e3aa3
NGottschewsky <nina.gottschewsky@gmail.com>
2019-12-06 14:58:08 +0100

    Correct orientations added

--- Unity_ET_VR/Assets/Scripts/ToolManager2.cs
+++ Unity_ET_VR/Assets/Scripts/ToolManager2.cs
@@ -16,14 +16,18 @@ public class ToolManager2 : MonoBehaviour
 
     [SerializeField] private List<ToolController> _tools;
     
-    [SerializeField] public GameObject spawnerPosition;
+    [SerializeField] public GameObject spawnerPositionLeft;
+    [SerializeField] public GameObject spawnerPositionRight;
+
+    private string[] _leftTools = new string[] {"1", "3", "5", "7", "9", "11", "13", "15", "17", "19", "21", "23"};
+    private string[] _rightTools = new string[] {"2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22", "24"};
     
     private int _trial;
 
     //private int[] _toolOrder;
     private List<string[]> _toolOrder = new List<string[]>();
     
-    private string _filePath = "D:\\Nina_ET_VR\\Other\\Test_CSV.csv";
+    private string _filePath = "D:\\Nina_ET_VR\\Experiment\\ET_VR_Tools\\PermutationMatrix\\ExperimentLoopMatrix.csv";
     
     private bool _endOfBlock = false;
 
@@ -114,9 +118,23 @@ public class ToolManager2 : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && !_endOfBlock) //will be replaced with some controller movement
         {
+            
             GetNextTool(out var internalTool);
-            ToolPresenter.INSTANCE.PresentTool(internalTool);
+            
+            if (Array.Exists(_leftTools, element => element == internalTool.id))
+            {
+                ToolPresenter.INSTANCE.PresentTool(internalTool, "left");
+            }
+            else if (Array.Exists(_rightTools, element => element == internalTool.id))
+            {
+                ToolPresenter.INSTANCE.PresentTool(internalTool, "right");
+            }
+            else
+            {
+                Debug.LogError("tool ID not contained in either list of tools");
+            }
             _trial++;
+            
         }
 
         if (_trial == (_toolOrder[ParticipantNr].Length))
--- Unity_ET_VR/Assets/Scripts/ToolPresenter.cs
+++ Unity_ET_VR/Assets/Scripts/ToolPresenter.cs
@@ -7,23 +7,6 @@ using UnityEngine.Serialization;
 
 public class ToolPresenter : MonoBehaviour
 {
-    private Vector3 position;
-    private Quaternion rotation;
-    private bool _isPresent;
-    private float[] _yCoordinate;
-
-    private void Awake()
-    {
-    }
-
-    void Start()
-    {
-        //_yCoordinate = new[] {0.7994252f, 0.8497297f};
-        position = new Vector3(0.5f, 0.7994252f, 0.0f);
-        rotation = new Quaternion(0, 0, 0, 0);
-        _isPresent = false;
-    }
-
 
     public static readonly ToolPresenter INSTANCE = new ToolPresenter();
 
@@ -36,11 +19,24 @@ public class ToolPresenter : MonoBehaviour
         get { return INSTANCE; }
     }
 
-    public void PresentTool(ToolController tool)
+    public void PresentTool(ToolController tool, string orientation)
     {
         ToolManager2.instance.DeactivateLastTool();
-        
-        tool.ActivateThis(ToolManager2.instance.spawnerPosition.transform.position, rotation);
+
+        if (orientation == "left")
+        {
+            tool.ActivateThis(ToolManager2.instance.spawnerPositionLeft.transform.position, ToolManager2.instance.spawnerPositionLeft.transform.rotation);
+            
+        }
+        else if(orientation == "right")
+        {
+            tool.ActivateThis(ToolManager2.instance.spawnerPositionRight.transform.position, ToolManager2.instance.spawnerPositionRight.transform.rotation);
+        }
+        else
+        {
+            Debug.LogError("wrong orientation");
+        }
+       
         
 
     }

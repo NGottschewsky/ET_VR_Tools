6e4da1981aea063a9b9e008f4a8ecb62f99ea115
Nina Gottschewsky <nina.gottschewsky@gmail.com>
2020-01-06 11:47:35 +0100

    Collider

---
+++ Unity_ET_VR/Assets/Grabable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabable : MonoBehaviour
+{
+    public bool LeftSide = false;
+
+    public Transform tool;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(steamVTInput.grab)
+        // grabbing
+        if (tool != null)
+        {
+            //do thing
+        }
+        throw new NotImplementedException();
+    }
+}

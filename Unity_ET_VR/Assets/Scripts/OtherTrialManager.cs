2416cba59d7de9757257a20f924ca81772a0bc44
NGottschewsky <nina.gottschewsky@gmail.com>
2020-02-07 14:08:33 +0100

    Next trial and table calbration

--- Unity_ET_VR/Assets/Scripts/OtherTrialManager.cs
+++ Unity_ET_VR/Assets/Scripts/OtherTrialManager.cs
@@ -1,13 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 public class OtherTrialManager : MonoBehaviour
 {
+    private bool _handInCollider = false;
+    private Interactable _interactable;
+    private bool _nextTrial = false;
+    public static OtherTrialManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        _interactable = this.GetComponent<Interactable>();
+    }
+
     private void OnHandHoverBegin( Hand hand )
     {
-         
+        if (hand == ToolManager2.instance.hand)
+        {
+            _handInCollider = true;
+        }
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+        _handInCollider = false;
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        if (_handInCollider)
+        {
+            if (ToolManager2.instance.grabPinch.state)
+            {
+                
+            }
+        }
     }
 
 }
